ccfc7cb4c75ebcd248d0ed88bf268e699cc6d587
Jeff Lowenhaupt <jeff.lowenhaupt@gmail.com>
2020-10-08 21:14:07 -0400

    start of schemas

--- index.js
+++ index.js
@@ -4,10 +4,71 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const userRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
+const mongoose = require('mongoose');
+const { url } = require('inspector');
 
-app.listen(PORT, () => {
-    console.log(`App listening at port ${PORT}`)
-})
+
+// connect to the MongoDB server
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+      //const regex = //(http:\/\/|https:\/\/)\w+.\w+(\/|.)\w*(\/|.)\w*(\/|.)\w*(\/|.)\w*#?/;
+      return /(http:\/\/|https:\/\/)\w+.\w+(\/|.)\w*(\/|.)\w*(\/|.)\w*(\/|.)\w*#?/.test(v);
+      //return v.match(regex);
+    },
+    message: 'Sorry, this is not a valid url',
+  },
+}});
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    validate: {
+      validator(v) {
+      //const regex = //(http:\/\/|https:\/\/)\w+.\w+(\/|.)\w*(\/|.)\w*(\/|.)\w*(\/|.)\w*#?/;
+      return /(http:\/\/|https:\/\/)\w+.\w+(\/|.)\w*(\/|.)\w*(\/|.)\w*(\/|.)\w*#?/.test(v);
+      //return v.match(regex);
+      },
+    message: 'Sorry, this is not a valid url',
+  }
+  },
+  owner: {
+    //link to the card author's model, ObjectId type, required field
+  },
+  likes: {
+   //a list of users who liked the post, an ObjectId array, an empty array by default (default field)
+    },
+  createdAt: {
+    //creation date, Date type, default value Date.now
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,3 +78,10 @@ app.use("/", cardsRouter);
 app.use( (req, res) => {
   res.status(404).send({ message: "Requested Resource not found" });
 });
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`)
+})
+
+module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('card', cardSchema);
